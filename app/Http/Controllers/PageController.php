e0e748f987fe39aec079f7de483c8d163cd525e5
Jackline Mukami <jacklinekirimi6@gmail.com>
2024-06-20 22:49:48 -0700

    This is a ToDoApp

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -15,7 +15,14 @@ class PageController extends Controller
     public function contact(){
         return 'this is the contact page';
     }
-
-
+    public function products(){
+        return 'This are the products that are sold';
+    }
+    public function services(){
+        return 'This are the services offered';
+    }
+    public function blog(){
+        return 'This is the blog page';
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('App\Http\Controllers')->group(function() {
-    Route::get('/', 'PageController@home')->name('home');
+    Route::get('/home', 'PageController@home')->name('home');
     Route::get('/about', 'PageController@about')->name('about');
     Route::get('/contact', 'PageController@contact')->name('contact');
+    Route::get('/products', 'PageController@products')->name('products');
+    Route::get('/services', 'PageController@services')->name('services');
+    Route::get('/blog', 'PageController@blog')->name('blog');
  });
